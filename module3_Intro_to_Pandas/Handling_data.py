707659bbf9e8bc4c3aa151c7fcb508da21429bb5
UmairSaifi <umairsaifi2002@gmail.com>
2025-07-22 22:01:28 +0530

    Add initial implementation for handling missing values in Pandas DataFrame

---
+++ module3_Intro_to_Pandas/Handling_data.py
@@ -0,0 +1,22 @@
+# Handling data in Pandas
+import pandas as pd
+
+data = {
+    'name' : ['Alice', 'Bob', 'Charlie', 'David', 'Eva'],
+    'age' : [25, 30, None, 34, 29],
+    'city' : ['New York', 'Los Angeles', 'Chicago', 'Houstan' , 'Phoenix'],
+    'course' : ['Math', None, 'History', 'Art', 'Physics'],
+}
+
+df = pd.DataFrame(data)
+print(f'Original DataFrmae: \n{df}')
+
+# now we will learn how to handle the missing values in the DataFrmaes
+# first we will check for the missing values
+print(f'\nThe Missing values in the DataFrame : \n{df.isna()}')
+
+# if we donot want the missing values so we can frop it 
+print(f'\nDataFrame after dropping the missing values: \n{df.dropna()}')
+
+# 
+print(f'\nDataFrame after filling the missing values with 0: \n{df.fillna({"age": 19, "course": "Unknown"} )}')
